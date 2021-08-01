044098d1dd1fbd6bd812b853a070cc60b633bd5a
Deb <debkbanerji@gmail.com>
2021-07-31 20:01:14 -0700

    Example Call

--- app/js/index.js
+++ app/js/index.js
@@ -1,20 +1,42 @@
 const SpotifyWebApi = window.SpotifyWebApi;
+const clientId = CLIENT_ID;
 
-const scopes = ['playlist-modify-private'],
+// TODO: Check to see if token is still valid using timestamp?
+const accessTokenRegexMatch = /#access_token=(.*?)&/.exec(window.location.hash);
+const accessToken = accessTokenRegexMatch != null ? accessTokenRegexMatch[1] : null
+
+function login() {
+  const scopes = ['playlist-modify-private'];
   // TODO: Switch to website in console
-  redirectUri = 'https://localhost:8080',
-  clientId = CLIENT_ID,
-  redirectState = 'login-attempted',
-  showDialog = true,
-  responseType = 'token';
+  const redirectUri = 'https://localhost:8080';
+  const redirectState = 'login-attempted';
+  const showDialog = true;
+  const responseType = 'token';
 
-const spotifyApi = new SpotifyWebApi({
-  redirectUri,
-  clientId
-});
+  const authorizeURL = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&state=${redirectState}&show_dialog=${showDialog}`;
+  window.location.replace(authorizeURL);
+}
 
-const authorizeURL= `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&state=${redirectState}&show_dialog=${showDialog}`;
+if (accessToken == null) {
+  // TODO: Bind to button
+  login();
+} else {
+  // We're logged in - initialize API
+  const spotifyApi = new SpotifyWebApi({
+    // redirectUri,
+    clientId
+  });
+  spotifyApi.setAccessToken(accessToken);
 
-// window.location.replace("http://stackoverflow.com");
 
-console.log(authorizeURL);
+  // TODO: Replace example call with logic, pass to web worker?
+  // Get Elvis' albums
+  spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+    function(data) {
+      console.log('Artist albums', data.body);
+    },
+    function(err) {
+      console.error(err);
+    }
+  );
+}
